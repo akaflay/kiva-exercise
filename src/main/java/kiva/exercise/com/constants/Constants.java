314bc1f389d64083fcf6423edbd0491ae0135593
Angad <angad.kaflay@8x8.com>
2019-01-19 09:40:07 -0800

    Refactoring some code to extract to constants

--- src/main/java/kiva/exercise/com/constants/Constants.java
+++ src/main/java/kiva/exercise/com/constants/Constants.java
@@ -2,13 +2,19 @@ package kiva.exercise.com.constants;
 
 public class Constants {
 
-	Constants() {
+    Constants() {
 
-		throw new RuntimeException("Not Allowed to call the constructor");
-	}
+        throw new RuntimeException("Not Allowed to call the constructor");
+    }
 
-	public static final String DEFAULT_PATH="search";
-	public static final String DEFAULT_JSON_PATH=".json";
-	public static final String LOAN_PATH="loans";
+    public static final String DEFAULT_PATH = "search";
+    public static final String DEFAULT_JSON_PATH = ".json";
+    public static final String LOAN_PATH = "loans";
+    public static final String LENDERS_PATH = "lenders";
+    public static final String LENDER_ID_PATH = "lender_id";
+    public static final String FUNDED_AMOUNT_PATH = "funded_amount";
+    public static final String ANNONYMUS = "anonymous";
+    public static final Double DOUBLE_ZERO = 0.00;
+    public static final Integer INTEGER_ZERO = 0;
 
 }
